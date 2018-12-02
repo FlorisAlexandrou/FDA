0251f43736d1416dc1b5fd1e2ad1d14ed8b4f80e
Floris <FAlexandrou@uclan.ac.uk>
2018-12-02 22:17:34 +0200

    Added Start Function, implemented one universal function to clear displays.

--- th-testing-api.js
+++ th-testing-api.js
@@ -1,3 +1,19 @@
+// function sample() {
+//     var numOfThs = document.getElementById("listTestingNumber").value;
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function () {
+//         if (this.readyState === 4 && this.status === 200) {
+//             //TODO If response received (success).
+//         }
+//         else {
+//             //TODO If response not received (error).
+//         }
+//     };
+//     xhttp.open("GET", "https://codecyprus.org/th/test-api/list?number-of-ths=" + numOfThs, true);
+//     xhttp.send();
+// }
+
+
 function listTest() {
     var numOfThs = document.getElementById("listTestingNumber").value;
     var xhttp = new XMLHttpRequest();
@@ -6,11 +22,11 @@ function listTest() {
             //TODO If response received (success).
             var object = JSON.parse(this.responseText);
             console.log(object);
-            var listTesting = document.getElementById("listTesting");
-            listTesting.innerHTML = "";
+            var listTestingDisplay = document.getElementById("listTestingDisplay");
+            listTestingDisplay.innerHTML = "";
             for (let i = 0; i < object.treasureHunts.length; i++) {
 
-                listTesting.innerHTML += "<p>" + (i + 1) + ". " + JSON.stringify(object.treasureHunts[i], null , 4) + "<p/>";
+                listTestingDisplay.innerHTML += "<p>" + (i + 1) + ". " + JSON.stringify(object.treasureHunts[i], null , 4) + "<p/>";
 
             }
         }
@@ -25,7 +41,28 @@ function listTest() {
     xhttp.send();
 }
 
-function clearList() {
-    var listTesting = document.getElementById("listTesting");
-    listTesting.innerHTML = "Response will appear here :)";
+//Functions that clears divs' text by id.
+function clearDisplays(displayID) {
+    var listTesting = document.getElementById(displayID);
+    listTesting.innerHTML = "Display cleared :)";
+}
+
+function startTest() {
+    var sTestList = document.getElementById("startTestParameters");
+    var sTestParams = sTestList.options[sTestList.selectedIndex].value;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            //TODO If response received (success).
+            var object = JSON.parse(this.responseText);
+            var startTestingDisplay = document.getElementById("startTestingDisplay");
+            startTestingDisplay.innerHTML = "";
+            startTestingDisplay.innerHTML = JSON.stringify(object,null,4);
+        }
+        else {
+            //TODO If response not received (error).
+        }
+    };
+    xhttp.open("GET", "https://codecyprus.org/th/test-api/start?player=" + sTestParams , true);
+    xhttp.send();
 }
