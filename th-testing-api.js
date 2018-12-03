4992e49fc0fb9628e76ced100d02f969a0f65bff
Floris <FAlexandrou@uclan.ac.uk>
2018-12-03 17:00:50 +0200

    Added Question and Answer functions.

--- th-testing-api.js
+++ th-testing-api.js
@@ -66,3 +66,63 @@ function startTest() {
     xhttp.open("GET", "https://codecyprus.org/th/test-api/start?player=" + sTestParams , true);
     xhttp.send();
 }
+
+function questionTest() {
+    var thCompleted = false;
+    var canBeSkipped = false;
+    var requiresLocation = false;
+    var qTestList = document.getElementById("questionTestParameters");
+    var qType = qTestList.options[qTestList.selectedIndex].value;
+    if(document.getElementById("qthCompleted").checked === true)
+        thCompleted = true;
+    if(document.getElementById("canBeSkipped").checked === true)
+        canBeSkipped = true;
+    if(document.getElementById("requiresLocation").checked === true)
+        requiresLocation = true;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            //TODO If response received (success).
+            var object = JSON.parse(this.responseText);
+            var questionTestingDisplay = document.getElementById("questionTestingDisplay");
+            questionTestingDisplay.innerHTML = "";
+            questionTestingDisplay.innerHTML = JSON.stringify(object,null,4);
+        }
+        else {
+            //TODO If response not received (error).
+        }
+    };
+    xhttp.open("GET", "https://codecyprus.org/th/test-api/question?" +
+        "completed=" + thCompleted +
+        "&question-type=" + qType +
+        "&can-be-skipped=" + canBeSkipped +
+        "&requires-location=" + requiresLocation, true);
+    xhttp.send();
+}
+
+function answerTest() {
+    var correct = false;
+    var thCompleted = false;
+    if(document.getElementById("correct").checked === true)
+        correct = true;
+    if(document.getElementById("athCompleted").checked === true)
+        thCompleted = true;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            //TODO If response received (success).
+            var object = JSON.parse(this.responseText);
+            var questionTestingDisplay = document.getElementById("answerTestingDisplay");
+            questionTestingDisplay.innerHTML = "";
+            questionTestingDisplay.innerHTML = JSON.stringify(object,null,4);
+        }
+        else {
+            //TODO If response not received (error).
+        }
+    };
+    xhttp.open("GET", "https://codecyprus.org/th/test-api/answer?" +
+        "correct=" + correct +
+        "&completed=" + thCompleted, true);
+    xhttp.send();
+}
